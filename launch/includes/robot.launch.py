36dd8a7c39a91055fc06307a7412663111306014
Zoe Li <Zoe.Li@bshg.com>
2019-07-01 18:14:18 -0700

    first commit

---
+++ launch/includes/robot.launch.py
@@ -0,0 +1,27 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+	use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+	kobuki_navigation_prefix = get_package_share_directory('kobuki_navigation')
+	kobuki_urdf = os.path.join(kobuki_navigation_prefix,'urdf', 'kobuki.urdf')
+	return LaunchDescription([
+        DeclareLaunchArgument(
+           'use_sim_time', 
+           default_value=use_sim_time,
+           description='Use simulation (Gazebo) clock if true'),
+
+        Node(
+            package='robot_state_publisher',
+            node_executable='robot_state_publisher',
+            node_name="robot_state_publisher",
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}],
+			arguments=[kobuki_urdf])
+])
